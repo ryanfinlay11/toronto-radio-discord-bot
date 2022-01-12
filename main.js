22fd23c0deff5dfa60d1efdaf550d694ce8cc9e1
Ryan Finlay <77310650+finlay-ryan@users.noreply.github.com>
2022-01-12 02:21:12 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS], 
+                            partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER'],});
+
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+client.events = new Discord.Collection();
+
+['command_handler', 'event_handler'].forEach(handler => {
+    require(`./handlers/${handler}`)(client, Discord);
+});
+
+//YOUR TOKEN HERE
+client.login('');
